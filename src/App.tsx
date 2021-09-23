272f76abd5ac17c3cd74bc671f8003f10095bb13
Fred Leal <flm_@msn.com>
2021-09-23 11:44:47 -0300

    Component Header finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Header } from "./Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
 
