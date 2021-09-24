8c831d50873862f8bfad9454b1aa2e149dd0aa14
Fred Leal <flm_@msn.com>
2021-09-24 14:12:55 -0300

    Tabela de transações

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -27,7 +27,7 @@ export function Summary() {
                     <p>Entradas</p>
                     <img src={totalImg} alt="Total" />
                 </header>
-                <strong>R$1000,00</strong>
+                <strong>R$500,00</strong>
             </div>
         </Container>
     )
